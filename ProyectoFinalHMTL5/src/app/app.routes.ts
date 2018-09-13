0869dc3ccd45c84dbfe5228ba854a6998246a37b
arr331 <43158042+arr331@users.noreply.github.com>
2018-09-13 16:16:49 -0500

    m

--- ProyectoFinalHMTL5/src/app/app.routes.ts
+++ ProyectoFinalHMTL5/src/app/app.routes.ts
@@ -2,13 +2,10 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { OrgUnoComponent } from './components/org-uno/org-uno.component';
+//import { OrgUnoComponent } from './components/org-uno/org-uno.component';
 import { OrgDosComponent } from './components/org-dos/org-dos.component';
-import { TestComponent } from './components/test/test.component';
 import { PrincipalComponent } from './components/principal/principal.component';
-import { TestimoniosComponent } from './components/testimonios/testimonios.component';
 import { ServiciosComponent } from './components/servicios/servicios.component';
-import { EstudiantesComponent } from './components/estudiantes/estudiantes.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { AutoresComponent } from './components/autores/autores.component';
 import { LoginComponent } from './components/login/login.component';
@@ -17,7 +14,7 @@ import { LoginComponent } from './components/login/login.component';
 export const APPROUTER: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full'},
     { path: 'home', component: HomeComponent},
-    { path: 'orguno', component: OrgUnoComponent},
+    //{ path: 'orguno', component: OrgUnoComponent},
     { path: 'usuarios', component: OrgDosComponent},
     { path: 'login', component: LoginComponent},
     { path: '**', redirectTo: 'home', pathMatch: 'full'}   
