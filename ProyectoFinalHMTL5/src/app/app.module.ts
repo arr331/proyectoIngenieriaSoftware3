045a564f6f0ca2829416594d07fbf52b68cc8c14
arr331 <43158042+arr331@users.noreply.github.com>
2018-09-13 00:49:22 -0500

    GoogleMaps
    cambios

--- ProyectoFinalHMTL5/src/app/app.module.ts
+++ ProyectoFinalHMTL5/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { OrganizationChartModule, ButtonModule } from 'primeng/primeng';
 import { GrowlModule } from 'primeng/components/growl/growl';
-
+ import { AgmCoreModule } from '@agm/core'; 
 import { AppComponent } from './app.component';
 import { OrgUnoComponent } from "./components/org-uno/org-uno.component";
 import { HomeComponent } from './components/home/home.component';
@@ -19,6 +19,8 @@ import { AutoresComponent } from './components/autores/autores.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { TestimoniosComponent } from './components/testimonios/testimonios.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+// import { LoginComponent } from './components/src/app/components/login/login.component';
 
 
 @NgModule({
@@ -34,8 +36,10 @@ import { FooterComponent } from './components/footer/footer.component';
     AutoresComponent,
     ContactoComponent,
     TestimoniosComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ],
+  
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -43,6 +47,9 @@ import { FooterComponent } from './components/footer/footer.component';
     GrowlModule,
     ButtonModule,
     RouterModule.forRoot(APPROUTER),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAexdUynkyyvvyo1aPJAjlRZkzJO6WQL2M'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ProyectoFinalHMTL5/src/app/components/home/home.component.ts
+++ ProyectoFinalHMTL5/src/app/components/home/home.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  ubicaciones = [
+    {latitud: 232323, longitud:2323}, 
+    {latitud: 232323, longitud:2323},
+    {latitud: 234545323, longitud:4545},
+  ]
   constructor() { }
 
   ngOnInit() {
